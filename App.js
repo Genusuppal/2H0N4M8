ed195977baf45be45108787059840d8cd34e78c0
Genusuppal <harpranav2202@gmail.com>
2023-09-09 00:20:28 +0530

    First Commit

--- App.js
+++ App.js
@@ -1,20 +1,54 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, SafeAreaView, View, Button } from "react-native";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<SafeAreaView style={styles.container}>
+			<View>
+				<Text>Bingo?</Text>
+				<Text>Start playing NOW!</Text>
+			</View>
+			<View>
+				<Row id={0} />
+				<Row id={1} />
+				<Row id={2} />
+				<Row id={3} />
+				<Row id={4} />
+			</View>
+			<StatusBar style="auto" />
+		</SafeAreaView>
+	);
+}
+
+function Row({ id }) {
+	return (
+		<View style={styles.row}>
+			{[1, 2, 3, 4, 5].map((item) => {
+				return (
+					<Button key={item} style={styles.cell} title={JSON.stringify(id * 5 + item)}/>
+				);
+			})}
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		display: "flex",
+		height: "100%",
+		backgroundColor: "#fff",
+		alignItems: "center",
+		justifyContent: "space-evenly",
+	},
+	row: {
+		display: "flex",
+		flexDirection: "row",
+		width: "80%",
+		justifyContent: "space-between",
+		marginBottom: 40,
+	},
+	cell: {
+		width: "20%",
+		justifyContent: "center",
+	},
 });
